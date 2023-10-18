7e5d6bbb688055a68f6e936f798e098b838851e8
chaudhari-tusharr <tusharchaudhari641@gmail.com>
2023-10-18 17:38:00 +0530

    added

---
+++ index.java
@@ -0,0 +1,11 @@
+class A
+{
+    public static void main(String args[])
+    {
+        int a=10;
+        int b=20;
+        int c;
+        c=a+b;
+        System.out.println("Addition = "+c);
+    }
+}
